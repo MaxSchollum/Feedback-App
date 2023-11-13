62d2399d7a4bda19549ff2618d590d5dba931272
MaxSchollum <max.schollum@gmail.com>
2023-11-13 17:52:56 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+function App() {
+    const title = "Fuckoff"
+    const body = "youre annoying"
+    const comments = [
+        {id: 1, text: "comment 1"},
+        {id: 2, text: "comment 2"},
+        {id: 3, text: "comment 3"}
+    ]
+
+    const loading = false
+    const showComments = true
+
+    if (loading) return <h1>loading...</h1>
+
+    return ( 
+        <div className="container">
+            <h2>{title}</h2>
+            <p>{body}</p>
+            
+            {showComments ? (
+                <div className="comments">
+                <h3>Comments ({comments.length})</h3>
+                <ul>{comments.map((comment, index) => (
+                    <li key={index}>{comment.text}</li>
+                ))}</ul>
+            </div>
+            ) : "no"}
+
+            
+        </div>
+    )
+}
+
+export default App
