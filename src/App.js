66f67a2defab39a369c7f2a5938103d1f185911d
MaxSchollum <max.schollum@gmail.com>
2024-02-07 18:33:03 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,35 +1,12 @@
+import Header from "./Components/Header.JSX"
+
 function App() {
-    const title = "Fuckoff"
-    const body = "youre annoying"
-    const comments = [
-        {id: 1, text: "comment 1"},
-        {id: 2, text: "comment 2"},
-        {id: 3, text: "comment 3"}
-    ]
 
-    const loading = false
-    const showComments = true
-
-    const codeBlock = <div className="comments">
-                <h3>Comments ({comments.length})</h3>
-                <ul>{comments.map((comment, index) => (
-                    <li key={index}>{comment.text}</li>
-                ))}</ul>
-            </div>
-
-    if (loading) return <h1>loading...</h1>
-
-    return ( 
-        <div className="container">
-            <h2>{title}</h2>
-            <p>{body}</p>
-            
-            {showComments ? (codeBlock)
-            : "no"}
-
-            
-        </div>
-    )
+    return (<div className="Container">
+        <Header />
+        <h1> Get Ready
+        </h1>
+    </div>)
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import './index.css'
+import "./Index.css"
 import App from "./App";
 
+
 ReactDOM.render(
-    <React.StrictMode> 
+    <React.StrictMode>
         <App />
     </React.StrictMode>,
-    document.getElementById('root'))
+    document.getElementById("root"))
 
